1c15913b935025fcb5e0ba4322061dbcb504b931
pusit <pusit.niti59@gmail.com>
2025-07-24 15:28:25 +0700

    Setting slidebar

---
+++ components/slidebar/app-sidebar.tsx
@@ -0,0 +1,67 @@
+import { Calendar, Home, Inbox, Search, Settings } from "lucide-react"
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar"
+
+// Menu items.
+const items = [
+  {
+    title: "Overview",
+    url: "#",
+    icon: Home,
+  },
+  {
+    title: "Bookings management",
+    url: "#",
+    icon: Inbox,
+  },
+  {
+    title: "Interpreters management",
+    url: "#",
+    icon: Calendar,
+  },
+  {
+    title: "Reports",
+    url: "#",
+    icon: Search,
+  },
+  {
+    title: "Settings",
+    url: "#",
+    icon: Settings,
+  },
+]
+
+export function AppSidebar() {
+  return (
+    <Sidebar>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>AdminControl</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+  )
+}
