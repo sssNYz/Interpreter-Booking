1708205cf1b319e69408b1b9f6773a9f28baca8a
pusit <pusit.niti59@gmail.com>
2025-09-01 16:52:44 +0700

    Delete files not use

--- app/AdminPage/booking-manage/page.tsx
+++
@@ -1,7 +0,0 @@
-// app/admin/page.tsx
-import React from "react";
-import Booking from "@/components/AdminControls/booking-manage";
-
-export default function AdminPage() {
-  return <Booking />;
-}
--- app/AdminPage/page.tsx
+++
@@ -1,7 +0,0 @@
-// app/admin/page.tsx
-import React from "react";
-import Overview from "@/components/AdminControls/overview";
-
-export default function AdminPage() {
-  return <Overview />;
-}
